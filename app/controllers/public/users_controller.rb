b2af45d05b08572bffe8b60c62c5adfe4fa809ac
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-04-11 13:20:48 +0000

    会員view編集

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,16 +1,48 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_mypage_path
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
+    @user = current_user
   end
 
   def withdrawal
+    @user = current_user
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  #退会した会員は同じemailでログインできなくなる
+  def reject_inactive_user
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == true)
+        redirect_to new_user_registration_path
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
