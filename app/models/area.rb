9e3a10df72882b4ad003021db3ba0a259c21c7d2
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-04-15 13:02:21 +0000

    google map適用

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,7 +1,10 @@
 class Area < ApplicationRecord
-  
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   enum facilities: {コンビニ:0, ショッピング:1, 飲食店:2, 公園:3, その他:4}
-  
+
   belongs_to :user
 
   has_many :favorites, dependent: :destroy
